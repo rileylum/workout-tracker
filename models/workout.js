7b5eac74cabd90d75fddc283f0cb5c11991f4250
Riley Lum <rileylum@adam.com.au>
2021-10-07 17:55:15 +1030

    add Workout model

---
+++ models/workout.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                required: "Exercise type required"
+            },
+            name: {
+                type: String,
+                required: "Exercise name required"
+            },
+            distance: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            duration: {
+                type: Number
+            }
+        }
+    ]
+})
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
