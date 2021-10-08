3bf08355ac464cbf0c9e4e5b880a6652e7eee021
Riley Lum <rileylum@adam.com.au>
2021-10-09 09:24:09 +1030

    fix PORT env variable for heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
