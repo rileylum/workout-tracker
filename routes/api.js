a7b91dde60936b0bd7fe942c85427946da1dd388
Riley Lum <rileylum@adam.com.au>
2021-10-09 08:06:27 +1030

    add logic to workout get route

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,25 @@
 const router = require("express").Router();
+const Workout = require("../models/workout");
+
 // GET WORKOUTS
 router.get("/workouts", (req, res) => {
-    res.send("hello!");
+    Workout.aggregate([{
+        // sum the duration of all exercises and add a field totalDuration
+        $addFields: {
+            totalDuration: {$sum: "$exercises.duration"}
+        }
+    },
+    {
+        // sort oldest to newest
+        $sort: {day: 1}
+    }])
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    })
+    
 });
 // ADD EXERCISE TO CURRENT WORKOUT
 router.put("/workouts", (req, res) => {
