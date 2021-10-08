803d864b243a13746ddf8faf71c2bfb59ed21e5c
Riley Lum <rileylum@adam.com.au>
2021-10-09 08:56:10 +1030

    add logic for get workout by range

--- routes/api.js
+++ routes/api.js
@@ -45,9 +45,25 @@ router.post("/workouts", (req, res) => {
         res.status(400).json(err);
     })
 })
-// GET WORKOUTS FROM LAST 7 DAYS
+// GET LAST 7 WORKOUTS
 router.get("/workouts/range", (req, res) => {
-    res.send("hello");
+
+    Workout.aggregate([{
+        // sum the duration of all exercises and add a field totalDuration
+        $addFields: {
+            totalDuration: {$sum: "$exercises.duration"}
+        }
+    },
+    {
+        // sort oldest to newest
+        $sort: {day: 1}
+    }])
+    .then(dbWorkout => {
+        res.json(dbWorkout.slice(dbWorkout.length-7));
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    })
 })
 
 module.exports = router;
